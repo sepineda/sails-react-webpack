3c063e1bea975a8f05325a156181b477a6343885
Eduardo Pineda <sepineda@gmail.com>
2017-11-22 19:56:35 -0600

    Compiling with babel succesfully

---
+++ assets/dist/bundle.js
@@ -0,0 +1,32 @@
+webpackJsonp([1],{
+
+/***/ 14:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = __webpack_require__(7);
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log('hey there!!');
+var App = function App() {
+  return _react2.default.createElement(
+    'div',
+    null,
+    'Hello'
+  );
+};
+
+_reactDom2.default.render(_react2.default.createElement(App, null), document.getElementById('root'));
+
+/***/ })
+
+},[14]);
---
+++ assets/src/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+console.log('hey there!!');
+const App = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
