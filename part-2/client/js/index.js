d2092d0d8a94585599c4dc028cd1244841e4955a
Eduardo Pineda <eduardo.pineda@hpe.com>
2018-05-17 08:29:30 -0600

    Fixed wrong placement for Router

--- part-2/client/js/index.js
+++ part-2/client/js/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './components/app/App';
 
 render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <App />,
     document.getElementById('root')
 );
